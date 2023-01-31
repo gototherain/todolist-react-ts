77f6638d30c81394561328cf602887103fe5eabe
gototherain <go.to.the.rain@gmail.com>
2023-01-31 13:32:07 +0600

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
+import { TaskType, Todolist } from './Todolist';
 
 function App() {
+
+
+  let [tasks, setTasks] = useState([
+    { id: 1, title: "CSS&HTML", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false },
+    { id: 4, title: "Redux", isDone: false }
+  ]);
+
+  let [filter, setFilter] = useState("all");
+
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter((t) =>  t.id !== id)
+    setTasks(filteredTasks);
+  }
+
+  let tasksForTodoList = tasks;
+  if (filter === "completed") {
+    tasksForTodoList = tasks.filter(t => t.isDone === true);
+  }
+
+  if (filter === "active") {
+    tasksForTodoList = tasks.filter(t => t.isDone === false);
+  }
+
   return (
     <div className="App">
-      <Todolist />
-      <Todolist />
-      <Todolist />
-    </div>
-  );
-}
-
-function Todolist() {
-  return (
-    <div>
-      <h3>What to learn?</h3>
-      <div>
-        <input />
-        <button>+</button>
-      </div>
-      <ul>
-        <li><input type="checkbox" checked={true} /><span>CSS&HTML</span></li>
-        <li><input type="checkbox" checked={true} /><span>JS</span></li>
-        <li><input type="checkbox" checked={false} /><span>React</span></li>
-      </ul>
+      <Todolist title="What to learn"
+        tasks={tasksForTodoList}
+        removeTask={removeTask} />
     </div>
   );
 }
